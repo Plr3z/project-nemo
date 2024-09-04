f0097e0c0e7c4af5a24469ee2edce4053a0e7392
Plr3z <plrezende02@gmail.com>
2024-09-03 23:03:31 -0300

    Initial commit after reinitializing the repository

--- index.js
+++ index.js
@@ -8,6 +8,6 @@ app.use(express.json())
 app.use(cors())
 app.use(router)
 
-app.listen(4000,() => {
+app.listen(process.env.PORT || 4000,() => {
     console.log('Servidor rodando na porta 4000!')
 })
